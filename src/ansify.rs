b14a6d38c0915fa5762bae7665fdb13c1deb6ac4
widberg <jaredwidberg@gmail.com>
2022-12-09 19:35:38 -0500

    Split file

---
+++ src/ansify.rs
@@ -0,0 +1,256 @@
+use ansi_term::Colour::Fixed;
+use image::{RgbImage};
+use kd_tree::KdMap;
+use log::info;
+use serde::{Deserialize, Serialize};
+use std::collections::BTreeMap;
+use std::vec::Vec;
+use std::fs::File;
+use std::path::PathBuf;
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+pub struct Palette {
+    colors: Vec<[u8; 3]>,
+}
+
+impl Palette {
+    pub fn from(path: PathBuf) -> Result<Palette, Box<dyn std::error::Error>> {
+        info!("Opening and parsing palette");
+
+        let file = File::open(path)?;
+        return Ok(serde_yaml::from_reader(&file)?);
+    }
+}
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+pub struct Blocks {
+    width: u32,
+    height: u32,
+    blocks: BTreeMap<char, Vec<Vec<bool>>>,
+}
+
+impl Blocks {
+    pub fn from(path: PathBuf) -> Result<Blocks, Box<dyn std::error::Error>> {
+        info!("Opening and parsing blocks");
+
+        let file2 = File::open(path)?;
+        let blocks: Blocks = serde_yaml::from_reader(&file2)?;
+
+        info!("Verifying block dimensions");
+
+        for (_character, bitmap) in blocks.blocks.iter() {
+            assert!(bitmap.len() == blocks.height as usize);
+            for row in bitmap {
+                assert!(row.len() == blocks.width as usize);
+            }
+        }
+
+        return Ok(blocks);
+    }
+
+    pub fn width(&self) -> u32 {
+        self.width
+    }
+
+    pub fn height(&self) -> u32 {
+        self.height
+    }
+}
+
+struct Shade {
+    ratio: f32,
+    block: char,
+}
+
+struct Texel {
+    foreground_color: u8,
+    background_color: u8,
+    block: char,
+}
+
+fn count_foreground_pixels(bitmap: &Vec<Vec<bool>>) -> u32 {
+    return bitmap
+        .into_iter()
+        .flat_map(IntoIterator::into_iter)
+        .map(|x| *x as u32)
+        .sum();
+}
+
+fn blend_two_colors(color_a: &[f32; 3], color_b: &[f32; 3], ratio: f32) -> [f32; 3] {
+    return [
+        color_a[0] * ratio + color_b[0] * (1.0 - ratio),
+        color_a[1] * ratio + color_b[1] * (1.0 - ratio),
+        color_a[2] * ratio + color_b[2] * (1.0 - ratio),
+    ];
+}
+
+fn normalize_color(color: &[u8; 3]) -> [f32; 3] {
+    return [
+        color[0] as f32 / 255.0,
+        color[1] as f32 / 255.0,
+        color[2] as f32 / 255.0,
+    ];
+}
+
+pub struct ANSIfier {
+    palette: Palette,
+    pub blocks: Blocks,
+    kdtree: KdMap<[f32; 3], Texel>,
+}
+
+impl ANSIfier {
+    pub fn new(palette: Palette, blocks: Blocks) -> ANSIfier {
+        info!("Generating shades");
+
+        let mut shades = Vec::new();
+        for (character, bitmap) in blocks.blocks.iter() {
+            shades.push(Shade {
+                ratio: count_foreground_pixels(bitmap) as f32
+                    / (blocks.width * blocks.height) as f32,
+                block: *character,
+            });
+        }
+
+        info!("Generating texels");
+
+        let mut texels = Vec::new();
+
+        for shade in shades.iter() {
+            if shade.ratio == 0.0 {
+                for (i, color) in palette.colors.iter().enumerate() {
+                    texels.push((
+                        normalize_color(color),
+                        Texel {
+                            foreground_color: 0 as u8,
+                            background_color: i as u8,
+                            block: shade.block,
+                        },
+                    ));
+                }
+            } else if shade.ratio == 1.0 {
+                for (i, color) in palette.colors.iter().enumerate() {
+                    texels.push((
+                        normalize_color(color),
+                        Texel {
+                            foreground_color: i as u8,
+                            background_color: 0 as u8,
+                            block: shade.block,
+                        },
+                    ));
+                }
+            } else {
+                for (i, foreground_color) in palette.colors.iter().enumerate() {
+                    for (j, background_color) in palette.colors.iter().enumerate() {
+                        if foreground_color == background_color {
+                            continue;
+                        }
+                        let color = blend_two_colors(
+                            &normalize_color(foreground_color),
+                            &normalize_color(background_color),
+                            shade.ratio,
+                        );
+                        texels.push((
+                            color,
+                            Texel {
+                                foreground_color: i as u8,
+                                background_color: j as u8,
+                                block: shade.block,
+                            },
+                        ));
+                    }
+                }
+            }
+        }
+
+        info!("Generate kdtree");
+
+        return ANSIfier {
+            palette,
+            blocks,
+            #[cfg(feature = "rayon")]
+            kdtree: KdMap::par_build_by_ordered_float(texels),
+            #[cfg(not(feature = "std"))]
+            kdtree: KdMap::build_by_ordered_float(texels),
+        };
+    }
+
+    pub fn process(&self, img: &RgbImage) -> (RgbImage, String) {
+        info!("Creating output image");
+
+        let mut out = RgbImage::new(
+            img.width() * self.blocks.width,
+            img.height() * self.blocks.height,
+        );
+        let mut text = String::new();
+
+        info!("Generating output");
+
+        for (x, y, pixel) in img.enumerate_pixels() {
+            let nearest = self
+                .kdtree
+                .nearest(&[
+                    pixel.0[0] as f32 / 255.0,
+                    pixel.0[1] as f32 / 255.0,
+                    pixel.0[2] as f32 / 255.0,
+                ])
+                .unwrap()
+                .item;
+            let texel = &nearest.1;
+            text.push_str(
+                &Fixed(texel.foreground_color)
+                    .on(Fixed(texel.background_color))
+                    .paint(texel.block.to_string())
+                    .to_string(),
+            );
+
+            if x + 1 == img.width() {
+                text.push('\n');
+            }
+            let foreground_color = self.palette.colors[texel.foreground_color as usize];
+            let background_color = self.palette.colors[texel.background_color as usize];
+            for i in 0..self.blocks.width {
+                for j in 0..self.blocks.height {
+                    out.put_pixel(
+                        x * self.blocks.width + i,
+                        y * self.blocks.height + j,
+                        image::Rgb {
+                            0: if self.blocks.blocks[&texel.block][j as usize][i as usize] {
+                                foreground_color
+                            } else {
+                                background_color
+                            }
+                        },
+                    );
+                }
+            }
+        }
+
+        return (out, text);
+    }
+
+    pub fn calculate_new_dimensions(
+        &self,
+        original_dimensions: (u32, u32),
+        desired_dimensions: (Option<u32>, Option<u32>),
+    ) -> (u32, u32) {
+        info!("Calculating dimension and resizing");
+    
+        let ratio = (original_dimensions.0 as f32 / self.block_width() as f32)
+            / (original_dimensions.1 as f32 / self.block_height() as f32);
+    
+        return match desired_dimensions {
+            (None, None) => original_dimensions,
+            (Some(width), None) => (width, (width as f32 / ratio) as u32),
+            (None, Some(height)) => ((height as f32 * ratio) as u32, height),
+            (Some(width), Some(height)) => (width, height),
+        };
+    }
+
+    pub fn block_width(&self) -> u32 {
+        self.blocks.width()
+    }
+
+    pub fn block_height(&self) -> u32 {
+        self.blocks.height()
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,18 @@
-use ansi_term::Colour::Fixed;
+
 use clap::{Parser, Subcommand};
 use image::gif::{GifDecoder, GifEncoder, Repeat};
 use image::io::Reader as ImageReader;
-use image::AnimationDecoder;
-use image::Frame;
-use image::{DynamicImage, GenericImageView, RgbImage};
-use kd_tree::KdMap;
+use image::{AnimationDecoder, Delay, DynamicImage, Frame, GenericImageView};
 use log::info;
 use nokhwa::Camera;
-use serde::{Deserialize, Serialize};
 use show_image::create_window;
 use show_image::WindowOptions;
-use std::collections::BTreeMap;
 use std::fs::File;
 use std::path::PathBuf;
-use std::vec::Vec;
+use std::time::Instant;
+
+mod ansify;
+use ansify::{ANSIfier, Blocks, Palette};
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
@@ -60,239 +58,10 @@ enum Commands {
     Webcam {
         #[arg(short, long)]
         index: usize,
+
+        #[arg(short, long, value_name = "OUTPUT_PATH")]
+        output: Option<PathBuf>,
     },
-}
-
-#[derive(Debug, PartialEq, Serialize, Deserialize)]
-struct Palette {
-    colors: Vec<[u8; 3]>,
-}
-
-#[derive(Debug, PartialEq, Serialize, Deserialize)]
-struct Blocks {
-    width: u32,
-    height: u32,
-    blocks: BTreeMap<char, Vec<Vec<bool>>>,
-}
-
-struct Shade {
-    ratio: f32,
-    block: char,
-}
-
-struct Texel {
-    foreground_color: u8,
-    background_color: u8,
-    block: char,
-}
-
-fn count_foreground_pixels(bitmap: &Vec<Vec<bool>>) -> u32 {
-    return bitmap
-        .into_iter()
-        .flat_map(IntoIterator::into_iter)
-        .map(|x| *x as u32)
-        .sum();
-}
-
-fn blend_two_colors(color_a: &[f32; 3], color_b: &[f32; 3], ratio: f32) -> [f32; 3] {
-    return [
-        color_a[0] * ratio + color_b[0] * (1.0 - ratio),
-        color_a[1] * ratio + color_b[1] * (1.0 - ratio),
-        color_a[2] * ratio + color_b[2] * (1.0 - ratio),
-    ];
-}
-
-fn normalize_color(color: &[u8; 3]) -> [f32; 3] {
-    return [
-        color[0] as f32 / 255.0,
-        color[1] as f32 / 255.0,
-        color[2] as f32 / 255.0,
-    ];
-}
-
-struct ANSIfier {
-    palette: Palette,
-    blocks: Blocks,
-    kdtree: KdMap<[f32; 3], Texel>,
-}
-
-impl ANSIfier {
-    fn new(palette: Palette, blocks: Blocks) -> ANSIfier {
-        info!("Generating shades");
-
-        let mut shades = Vec::new();
-        for (character, bitmap) in blocks.blocks.iter() {
-            shades.push(Shade {
-                ratio: count_foreground_pixels(bitmap) as f32
-                    / (blocks.width * blocks.height) as f32,
-                block: *character,
-            });
-        }
-
-        info!("Generating texels");
-
-        let mut texels = Vec::new();
-
-        for shade in shades.iter() {
-            if shade.ratio == 0.0 {
-                for (i, color) in palette.colors.iter().enumerate() {
-                    texels.push((
-                        normalize_color(color),
-                        Texel {
-                            foreground_color: 0 as u8,
-                            background_color: i as u8,
-                            block: shade.block,
-                        },
-                    ));
-                }
-            } else if shade.ratio == 1.0 {
-                for (i, color) in palette.colors.iter().enumerate() {
-                    texels.push((
-                        normalize_color(color),
-                        Texel {
-                            foreground_color: i as u8,
-                            background_color: 0 as u8,
-                            block: shade.block,
-                        },
-                    ));
-                }
-            } else {
-                for (i, foreground_color) in palette.colors.iter().enumerate() {
-                    for (j, background_color) in palette.colors.iter().enumerate() {
-                        if foreground_color == background_color {
-                            continue;
-                        }
-                        let color = blend_two_colors(
-                            &normalize_color(foreground_color),
-                            &normalize_color(background_color),
-                            shade.ratio,
-                        );
-                        texels.push((
-                            color,
-                            Texel {
-                                foreground_color: i as u8,
-                                background_color: j as u8,
-                                block: shade.block,
-                            },
-                        ));
-                    }
-                }
-            }
-        }
-
-        info!("Generate kdtree");
-
-        return ANSIfier {
-            palette,
-            blocks,
-            kdtree: KdMap::par_build_by_ordered_float(texels),
-        };
-    }
-
-    fn process(&self, img: &RgbImage) -> (RgbImage, String) {
-        info!("Creating output image");
-
-        let mut out = RgbImage::new(
-            img.width() * self.blocks.width,
-            img.height() * self.blocks.height,
-        );
-        let mut text = String::new();
-
-        info!("Generating output");
-
-        for (x, y, pixel) in img.enumerate_pixels() {
-            let nearest = self
-                .kdtree
-                .nearest(&[
-                    pixel.0[0] as f32 / 255.0,
-                    pixel.0[1] as f32 / 255.0,
-                    pixel.0[2] as f32 / 255.0,
-                ])
-                .unwrap()
-                .item;
-            let texel = &nearest.1;
-            text.push_str(
-                &Fixed(texel.foreground_color)
-                    .on(Fixed(texel.background_color))
-                    .paint(texel.block.to_string())
-                    .to_string(),
-            );
-
-            if x + 1 == img.width() {
-                text.push('\n');
-            }
-            for i in 0..self.blocks.width {
-                for j in 0..self.blocks.height {
-                    out.put_pixel(
-                        x * self.blocks.width + i,
-                        y * self.blocks.height + j,
-                        if self.blocks.blocks[&texel.block][j as usize][i as usize] {
-                            let foreground_color =
-                                self.palette.colors[texel.foreground_color as usize];
-                            image::Rgb {
-                                0: foreground_color,
-                            }
-                        } else {
-                            let background_color =
-                                self.palette.colors[texel.background_color as usize];
-                            image::Rgb {
-                                0: background_color,
-                            }
-                        },
-                    );
-                }
-            }
-        }
-
-        return (out, text);
-    }
-}
-
-fn calculate_new_dimensions(
-    original_dimensions: (u32, u32),
-    new_dimensions: (Option<u32>, Option<u32>),
-    block_dimensions: (u32, u32),
-) -> (u32, u32) {
-    info!("Calculating dimension and resizing");
-
-    let ratio = (original_dimensions.0 as f32 / block_dimensions.0 as f32)
-        / (original_dimensions.1 as f32 / block_dimensions.1 as f32);
-
-    return match new_dimensions {
-        (None, None) => original_dimensions,
-        (Some(width), None) => (width, (width as f32 / ratio) as u32),
-        (None, Some(height)) => ((height as f32 * ratio) as u32, height),
-        (Some(width), Some(height)) => (width, height),
-    };
-}
-
-impl Palette {
-    fn from(path: PathBuf) -> Result<Palette, Box<dyn std::error::Error>> {
-        info!("Opening and parsing palette");
-
-        let file = File::open(path)?;
-        return Ok(serde_yaml::from_reader(&file)?);
-    }
-}
-
-impl Blocks {
-    fn from(path: PathBuf) -> Result<Blocks, Box<dyn std::error::Error>> {
-        info!("Opening and parsing blocks");
-
-        let file2 = File::open(path)?;
-        let blocks: Blocks = serde_yaml::from_reader(&file2)?;
-
-        info!("Verifying block dimensions");
-
-        for (_character, bitmap) in blocks.blocks.iter() {
-            assert!(bitmap.len() == blocks.height as usize);
-            for row in bitmap {
-                assert!(row.len() == blocks.width as usize);
-            }
-        }
-
-        return Ok(blocks);
-    }
 }
 
 #[show_image::main]
@@ -303,7 +72,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let palette = Palette::from(cli.palette)?;
     let blocks = Blocks::from(cli.blocks)?;
-    let block_dimensions = (blocks.width, blocks.height);
     let ansifier = ANSIfier::new(palette, blocks);
 
     match &cli.command {
@@ -317,10 +85,9 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             let original_image = ImageReader::open(input)?.decode()?;
 
             info!("Calculating dimension and resizing");
-            let new_dimensions = calculate_new_dimensions(
+            let new_dimensions = ansifier.calculate_new_dimensions(
                 original_image.dimensions(),
                 (cli.width, cli.height),
-                block_dimensions,
             );
             let img = original_image
                 .resize_exact(
@@ -348,8 +115,8 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                 let window = create_window(
                     "img2ansi",
                     WindowOptions::new().set_size([
-                        new_dimensions.0 * block_dimensions.0,
-                        new_dimensions.1 * block_dimensions.1,
+                        new_dimensions.0 * ansifier.block_width(),
+                        new_dimensions.1 * ansifier.block_height(),
                     ]),
                 )?;
                 window.set_image("image", out)?;
@@ -373,10 +140,9 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                 let delay = frame.delay();
                 let original_image = DynamicImage::ImageRgba8(frame.into_buffer());
 
-                let new_dimensions = calculate_new_dimensions(
+                let new_dimensions = ansifier.calculate_new_dimensions(
                     original_image.dimensions(),
                     (cli.width, cli.height),
-                    block_dimensions,
                 );
                 let img = original_image
                     .resize_exact(
@@ -401,7 +167,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                 ))?;
             }
         }
-        Commands::Webcam { index } => {
+        Commands::Webcam { index, output } => {
             info!("Creating webcam");
             let mut camera = Camera::new(*index, None)?;
             camera.open_stream()?;
@@ -411,10 +177,9 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
             info!("Calculating dimension and resizing");
 
-            let new_dimensions = calculate_new_dimensions(
+            let new_dimensions = ansifier.calculate_new_dimensions(
                 original_image.dimensions(),
                 (cli.width, cli.height),
-                block_dimensions,
             );
 
             info!("Creating image window");
@@ -422,10 +187,23 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             let window = create_window(
                 "img2ansi",
                 WindowOptions::new().set_size([
-                    new_dimensions.0 * block_dimensions.0,
-                    new_dimensions.1 * block_dimensions.1,
+                    new_dimensions.0 * ansifier.block_width(),
+                    new_dimensions.1 * ansifier.block_height(),
                 ]),
             )?;
+
+            let mut encoder = if let Some(output_file) = output {
+                info!("Gif file");
+
+                let file_out = File::create(output_file)?;
+                let mut new_encoder = GifEncoder::new(file_out);
+                new_encoder.set_repeat(Repeat::Infinite)?;
+                Some(new_encoder)
+            } else {
+                None
+            };
+
+            let mut last_frame = (None, Instant::now());
 
             loop {
                 let original_image = camera.frame()?;
@@ -441,6 +219,22 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                 let (out, _) = (&ansifier).process(&img);
 
                 info!("Showing image");
+
+                if let Some(ref mut enc) = encoder {
+                    if let (Some(real_last_frame), last_time) = last_frame {
+                        enc.encode_frame(Frame::from_parts(
+                            real_last_frame,
+                            0,
+                            0,
+                            Delay::from_saturating_duration(last_time.elapsed()),
+                        ))?;
+                    }
+
+                    last_frame = (
+                        Some(DynamicImage::ImageRgb8(out.clone()).to_rgba8()),
+                        Instant::now(),
+                    );
+                }
 
                 if window.set_image("image", out).is_err() {
                     info!("Closing window");
